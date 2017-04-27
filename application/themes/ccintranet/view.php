4c013640188373d44b94485487e1c1ebf556f086
William Bargent <w.bargent@canterburycollege.ac.uk>
2017-04-27 09:01:15 +0000

    Fix double login

--- application/themes/ccintranet/view.php
+++ application/themes/ccintranet/view.php
@@ -2,4 +2,8 @@
 
 $is_concrete5 = TRUE;
 
+Loader::element('header_required');
+
 include('/var/www/html/intranet/application/elements/theme/global/ResponsiveLoginDesign/concrete5.php');
+
+Loader::element('footer_required');
