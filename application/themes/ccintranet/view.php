c389c27346b49392b61b219c895cbee83f57f104
William Bargent <w.bargent@canterburycollege.ac.uk>
2017-05-05 08:51:23 +0000

    Fix Jquery issue at login

--- application/themes/ccintranet/view.php
+++ application/themes/ccintranet/view.php
@@ -1,8 +1,11 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
-$is_concrete5 = TRUE;
+$is_concrete5 = TRUE; ?>
 
-Loader::element('header_required');
+<!-- Jquery Order Matters -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js"></script>
+
+<?php Loader::element('header_required');
 
 include('/var/www/html/intranet/application/elements/theme/global/ResponsiveLoginDesign/concrete5.php');
 
